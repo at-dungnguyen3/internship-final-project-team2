f2b5b4f6338c7167814148ae4a72942065b56215
at-dungnguyen3 <dung.nguyen3@asiantech.vn>
2018-08-07 16:33:17 +0700

    Add basic sign up

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  before_save :downcase_email
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+  private
+
+    def downcase_email
+      self.email = email.downcase
+    end
+end
