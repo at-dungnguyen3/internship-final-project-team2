d0f7f47ac185b8fa0bee272a1391e933a75ac150
Chuong Phu <chuong.phu@asiantech.vn>
2018-08-17 15:18:39 +0700

    Change Method Time To Background Folder

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def format_time_to_seconds(time)
-    time = time.strftime('%H:%M:%S').split(':')
-    time[0].to_i * 3600 + time[1].to_i * 60 + time[2].to_i
-  end
 end
---
+++ lib/backgrounds/format_time_to_seconds.rb
@@ -0,0 +1,6 @@
+class FormatTimeToSeconds
+  def self.format_time_to_seconds(time)
+    time = time.strftime('%H:%M:%S').split(':')
+    time[0].to_i * 3600 + time[1].to_i * 60 + time[2].to_i
+  end
+end
