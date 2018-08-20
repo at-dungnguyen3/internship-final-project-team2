beaf2f28350095e0fd1a9fa64bd9dd29ae05eb2c
at-dungnguyen3 <dung.nguyen3@asiantech.vn>
2018-08-20 11:20:17 +0700

    Add function add and update auction in redis

--- app/controllers/admin/auctions_controller.rb
+++ app/controllers/admin/auctions_controller.rb
@@ -15,10 +15,13 @@ module Admin
 
     def create
       @auction = @product.auctions.build(auction_params)
-      respond_to do |format|
-        if @auction.save
+      if @auction.save
+        add_to_redis(@auction) if @auction.status == 1
+        respond_to do |format|
           format.html { redirect_to admin_product_auctions_url(@product.id), notice: 'Thêm thông tin đấu giá thành công' }
-        else
+        end
+      else
+        respond_to do |format|
           format.html { render :new }
         end
       end
@@ -29,6 +32,7 @@ module Admin
     def update
       respond_to do |format|
         if @auction.update(auction_params)
+          update_auction_from_redis(@auction)
           format.html { redirect_to admin_product_auctions_url(@auction.product.id), notice: 'Cập nhập thông tin đấu giá thành công' }
         else
           format.html { render :edit }
@@ -38,7 +42,7 @@ module Admin
 
     def destroy
       respond_to do |format|
-        if @auction.destroy
+        if @auction.status.zero? && @auction.destroy
           format.html { redirect_to admin_product_auctions_url(@auction.product.id), notice: 'Xóa thành công' }
         else
           format.html { redirect_to admin_product_auctions_url(@auction.product.id), notice: 'Xóa không thành công' }
@@ -59,5 +63,56 @@ module Admin
       def find_auction
         (@auction = Auction.find_by(id: params[:id])) || not_found
       end
-  end
+
+      def add_to_redis(auction)
+        period = format_time_to_seconds(auction.period)
+        data = {
+          id: auction.id,
+          start_at: auction.start_at,
+          end_at: auction.end_at,
+          period: period,
+          bid_step: auction.bid_step,
+          status: auction.status,
+          product_id: auction.product_id,
+          product_name: auction.product.name,
+          product_price: auction.product.price,
+          product_quantity: auction.product.quantity,
+          product_description: auction.product.description,
+          product_pictures: auction.product.pictures,
+          product_category: auction.product.category_id
+        }
+        $redis.set(auction.id, data.to_json)
+      end
+
+      def update_auction_from_redis(auction)
+        if $redis.get(auction.id)
+          if auction_params['status'] == 0
+            $redis.del(auction.id)
+          else
+            data = JSON.parse($redis.get(auction.id))
+            period = format_time_to_seconds(auction.period)
+            data['start_at'] = auction.start_at
+            data['end_at'] = auction.end_at
+            data['bid_step'] = auction.bid_step
+            data['product_id'] = auction.product_id
+            data['product_name'] = auction.product.name
+            data['product_price'] = auction.product.price
+            data['product_description'] = auction.product.description
+            data['product_quantity'] = auction.product.quantity
+            data['product_pictures'] = auction.product.pictures
+            data['product_category'] = auction.product.category_id
+            data['period'] = period
+            data['status'] = auction.status
+            $redis.set(auction.id, data.to_json)
+          end
+        else
+          add_to_redis(@auction) if auction_params['status'] == 1
+        end
+      end
+
+      def format_time_to_seconds(time)
+        time = time.strftime('%H:%M:%S').split(':')
+        time[0].to_i * 3600 + time[1].to_i * 60 + time[2].to_i
+      end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,85 +12,84 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_092455) do
-
-  create_table "auction_details", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "auction_id"
-    t.integer "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["auction_id"], name: "index_auction_details_on_auction_id"
+ActiveRecord::Schema.define(version: 20_180_815_092_455) do
+  create_table 'auction_details', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.bigint 'auction_id'
+    t.integer 'status'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['auction_id'], name: 'index_auction_details_on_auction_id'
   end
 
-  create_table "auctions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "product_id"
-    t.time "start_at"
-    t.time "end_at"
-    t.time "period"
-    t.integer "bid_step", default: 0
-    t.integer "status", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_auctions_on_product_id"
+  create_table 'auctions', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.bigint 'product_id'
+    t.time 'start_at'
+    t.time 'end_at'
+    t.time 'period'
+    t.integer 'bid_step', default: 0
+    t.integer 'status', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['product_id'], name: 'index_auctions_on_product_id'
   end
 
-  create_table "bids", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "auction_detail_id"
-    t.bigint "user_id"
-    t.integer "amount"
-    t.integer "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["auction_detail_id"], name: "index_bids_on_auction_detail_id"
-    t.index ["user_id"], name: "index_bids_on_user_id"
+  create_table 'bids', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.bigint 'auction_detail_id'
+    t.bigint 'user_id'
+    t.integer 'amount'
+    t.integer 'status'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['auction_detail_id'], name: 'index_bids_on_auction_detail_id'
+    t.index ['user_id'], name: 'index_bids_on_user_id'
   end
 
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.bigint "categories_id"
-    t.index ["categories_id"], name: "index_categories_on_categories_id"
-    t.index ["name", "categories_id"], name: "index_categories_on_name_and_categories_id", unique: true
+  create_table 'categories', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.string 'name', null: false
+    t.bigint 'categories_id'
+    t.index ['categories_id'], name: 'index_categories_on_categories_id'
+    t.index ['name', 'categories_id'], name: 'index_categories_on_name_and_categories_id', unique: true
   end
 
-  create_table "pictures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "product_id"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_pictures_on_product_id"
+  create_table 'pictures', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.bigint 'product_id'
+    t.string 'image'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['product_id'], name: 'index_pictures_on_product_id'
   end
 
-  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "description", null: false
-    t.string "detail", null: false
-    t.integer "quantity", default: 0
-    t.integer "price", default: 0
-    t.bigint "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_products_on_category_id"
+  create_table 'products', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'description', null: false
+    t.string 'detail', null: false
+    t.integer 'quantity', default: 0
+    t.integer 'price', default: 0
+    t.bigint 'category_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['category_id'], name: 'index_products_on_category_id'
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email"
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "password_digest"
-    t.integer "role", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "activation_digest"
-    t.boolean "activated?", default: false
-    t.datetime "activated_at"
-    t.string "remember_digest"
-    t.string "provider"
-    t.string "uid"
+  create_table 'users', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.string 'email'
+    t.string 'first_name', null: false
+    t.string 'last_name', null: false
+    t.string 'password_digest'
+    t.integer 'role', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'activation_digest'
+    t.boolean 'activated?', default: false
+    t.datetime 'activated_at'
+    t.string 'remember_digest'
+    t.string 'provider'
+    t.string 'uid'
   end
 
-  add_foreign_key "auction_details", "auctions"
-  add_foreign_key "auctions", "products"
-  add_foreign_key "bids", "auction_details"
-  add_foreign_key "bids", "users"
-  add_foreign_key "products", "categories"
+  add_foreign_key 'auction_details', 'auctions'
+  add_foreign_key 'auctions', 'products'
+  add_foreign_key 'bids', 'auction_details'
+  add_foreign_key 'bids', 'users'
+  add_foreign_key 'products', 'categories'
 end
